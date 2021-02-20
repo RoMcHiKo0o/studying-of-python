24898f1de3342dc99e1ca2beaaa063701c188ef6
RoMcHiKo0o <roman.golubovich@gmail.com>
2021-02-20 11:20:33 +0300

    rename funcs and remove function call

--- nodinok.py
+++ nodinok.py
@@ -1,4 +1,4 @@
-def nod(a, b):
+def gcd(a, b):
 	if type(a) == int and  type(b) == int: #проверка на тип int
 		if a < b:
 			a, b = b, a # сделал чтоб a > b
@@ -12,12 +12,8 @@ def nod(a, b):
 		return nod
 	else:
 		return "input error, type of numbers should be int"
-def nok(a, b):
+def lcm(a, b):
 	if type(a) == int and  type(b) == int:# такая же проверка как и в НОД
-		return int(a * b / nod(a, b))
+		return int(a * b / gcd(a, b))
 	else:
 		return ""
-a, b = int(input()), int(input())
-c, d = 4.1, 5 # создано специально чтоб словить ошибку
-print (nod(a, b), nok(a, b))
-print (nod(c, d), nok(c, d))

392050c172fad5b37655041c12a46c93e8cfae89
RoMcHiKo0o <roman.golubovich@gmail.com>
2021-03-16 17:54:09 +0300

    added nice output

--- nodinok.py
+++ nodinok.py
@@ -1,19 +1,39 @@
-def gcd(a, b):
-	if type(a) == int and  type(b) == int: #проверка на тип int
-		if a < b:
-			a, b = b, a # сделал чтоб a > b
-		if a != b: 
-			while b > 0:
-				a, b = b, a % b
-				#алгоритм евклида. a становится b, b становится остатком
-		else:
-			nod = a
-		nod = a
-		return nod
-	else:
-		return "input error, type of numbers should be int"
-def lcm(a, b):
-	if type(a) == int and  type(b) == int:# такая же проверка как и в НОД
-		return int(a * b / gcd(a, b))
-	else:
-		return ""
+import sys
+
+
+def gcd(a: int, b: int) -> int:
+    if (a**2 + b**2) > 0:
+        a, b = max(abs(a), abs(b)), min(abs(a), abs(b))
+        # тут я сделал a > b и сделал их положительными,
+        # потому что прога не проходила тест на отрицательные числа
+        # нод(a, b) = нод(-a, b)
+        if a != b:
+            while b > 0:
+                a, b = b, a % b
+                # a становится b, b становится остатком
+        else:
+            nod = a
+        nod = a
+        return nod
+    else:
+        return 0
+
+
+def lcm(a: int, b: int) -> int:
+    if a * b == 0:
+        return 0
+    else:
+        return abs(a * b) // gcd(a, b)
+
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) == 3:
+            a = int(sys.argv[1])
+            b = int(sys.argv[2])
+        else:
+            a = int(input())
+            b = int(input())
+        print(a, b, '->', gcd(a, b), lcm(a, b))
+    except ValueError:
+        print("Incorrect input!!!")
